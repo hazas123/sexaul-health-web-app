03c00900948b81c25058f306b7c7312ddbbb085a
James Smith <james.smith@holidayextras.com>
2018-02-02 16:42:18 +0000

    My local

--- server/main.js
+++ server/main.js
@@ -3,3 +3,27 @@ import { Meteor } from 'meteor/meteor'
 Meteor.startup(() => {
   // code to run on server at startup
 })
+
+Meteor.methods({
+  'callAPI': function () {
+    var apiai = require('apiai')
+
+    var app = apiai('')
+
+    var options = {
+      sessionId: '12'
+    }
+
+    var request = app.textRequest('hi', options)
+
+    request.on('response', function (response) {
+      console.log('RES', response.result.fulfillment.speech)
+      return response.result.fulfillment.speech
+    })
+    request.on('error', function (error) {
+      console.log('ERROR', error)
+      throw new Meteor.Error(error)
+    })
+    request.end()
+  }
+})
