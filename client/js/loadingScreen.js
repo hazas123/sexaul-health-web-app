368342b925137bf1ec6b8b9813013e84df6c5f63
Harry Andrew Brian Potter <harrypotter@harrys-mbp.home>
2018-02-05 18:13:46 +0000

    withModelsMale

--- client/js/loadingScreen.js
+++ client/js/loadingScreen.js
@@ -1,12 +1,27 @@
 import { Template } from 'meteor/templating'
 import { Session } from 'meteor/session'
+import { Blaze } from 'meteor/blaze'
 import '../templates/loadingScreen.html'
 
-Session.setDefault('myTemplate', 'hello')
+var view
+
+Template.loadingScreen.onRendered(function () {
+  view = this.view
+})
 
 Template.loadingScreen.events({
 
   'click button': function (event) {
-    Session.set('myTemplate', 'hello0')
+    $('#Hold').animate({
+      'marginLeft': '-=100vw'}, 4000)
+    $('body').css('overflow-y', 'visible')
+    setTimeout(() => Blaze.remove(view), 3400)
   }
 })
+
+Template.loadingScreen.onDestroyed(function () {
+  $('#Hold').remove()
+  console.log('BOOM')
+}
+
+)
