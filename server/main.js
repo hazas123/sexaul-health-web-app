074356b3e8c8f4482083a69633ad69d27209298e
James Smith <james.smith@holidayextras.com>
2018-02-28 13:32:18 +0000

    chatbot done

--- server/main.js
+++ server/main.js
@@ -5,12 +5,12 @@ Meteor.startup(() => {
 })
 
 Meteor.methods({
-  callAPI: function (val) {
+  callAPI: function (val, uuid) {
     return new Promise((resolve, reject) => {
       var apiai = require('apiai')
       var app = apiai('ae024febf4ff42c9930027e13f69cd28')
       var options = {
-        sessionId: '12'
+        sessionId: uuid
       }
       var request = app.textRequest(val, options)
       request.on('response', function (response) {
