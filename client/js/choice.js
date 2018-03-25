7ab4e127e233419eaa8aeb903b061d19db9cd364
Harry Andrew Brian Potter <hp237@kent.ac.uk>
2018-03-25 22:09:09 +0100

    changingResponsive

--- client/js/choice.js
+++ client/js/choice.js
@@ -34,14 +34,14 @@ Template.choice.events({
   'click #MenuLink1': function (event) {
     $('#MainMenuOption1').css('visibility', 'visible')
     console.log('area visible')
-  },
+  }/*,
   'click #closeMainMenuOption1': function (event) {
     $('#MainMenuOption1').css('visibility', 'hidden')
   },
   'click #closeMainMenuOption2': function (event) {
     $('#MainMenuOption2').css('visibility', 'hidden')
   }
-  /* 'click #SelectOptionCol4' (event, instance) {
+   'click #SelectOptionCol4' (event, instance) {
     // clicked the .clickbtn class
     // increment the counter when button is clicked
     $('#OptionsHolder').css('visibility', 'visible')
