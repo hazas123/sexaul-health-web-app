d059bd9f3f47b79a5f5105f65329c110a66397d0
James Smith <james.smith@holidayextras.com>
2018-03-28 16:14:40 +0100

    enter on chat bot

--- client/js/chatbox.js
+++ client/js/chatbox.js
@@ -25,30 +25,39 @@ Template.chatbox.events({
     $('.reply').remove()
     $('.chatlogs').append('<div class="chat reply"><p class="chat-message">I can help by listing STI symptoms. Is this something I can help you with?</p></div>')
   },
-  'click .send' (event, instance) {
-    var val = $.trim($('textarea').val())
-    $('textarea').val('')
-    var errorset = $('#chat-input').hasClass('errorValidation')
-    if (errorset) {
-      $('#chat-input').toggleClass('errorValidation')
+  'click .send, keyup #chat-input' (event, instance) {
+    if ((event.type === 'click') || (event.type === 'keyup' && event.which === 13)) {
+      var val = $.trim($('textarea').val())
+      $('textarea').val('')
+      var errorset = $('#chat-input').hasClass('errorValidation')
+      if (errorset) {
+        $('#chat-input').toggleClass('errorValidation')
+      }
+      $('.validation').css('visibility', 'hidden')
+      var uuid = Session.get('uniqueSessioID')
+      var clientCheck = /^[a-zA-Z0-9 .-?]+$/.test(val)
+      if (clientCheck) {
+        $('.chatlogs').append('<div class="chat self"><p class="chat-message">' + val + '</p></div>')
+        console.log(this)
+        Meteor.call('callAPI', val, uuid, function (error, result) {
+          if (error) {
+            console.log(error)
+          }
+          console.log(result)
+          $('.chatlogs').append('<div class="chat reply"><p class="chat-message">' + result + '</p></div>')
+        })
+        $('.chatlogs').animate({scrollTop: $('.chatlogs').prop('scrollHeight')}, 1000)
+      } else {
+        $('#chat-input').toggleClass('errorValidation')
+        $('.validation').css('visibility', 'visible')
+      }
     }
-    $('.validation').css('visibility', 'hidden')
-    var uuid = Session.get('uniqueSessioID')
-    var clientCheck = /^[a-zA-Z0-9 .-?]+$/.test(val)
-    if (clientCheck) {
-      $('.chatlogs').append('<div class="chat self"><p class="chat-message">' + val + '</p></div>')
-      console.log(this)
-      Meteor.call('callAPI', val, uuid, function (error, result) {
-        if (error) {
-          console.log(error)
-        }
-        console.log(result)
-        $('.chatlogs').append('<div class="chat reply"><p class="chat-message">' + result + '</p></div>')
-      })
-      $('.chatlogs').animate({scrollTop: $('.chatlogs').prop('scrollHeight')}, 1000)
-    } else {
-      $('#chat-input').toggleClass('errorValidation')
-      $('.validation').css('visibility', 'visible')
+  },
+  'keyup #chat-input' (event) {
+    if (event.which === 13) {
+      console.log('you hit enter')
+      event.stopPropagation()
+      return false
     }
   }
 })
